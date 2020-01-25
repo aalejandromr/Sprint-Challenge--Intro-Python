85b601a65b774142bc4efbf64c2fc3c2e8c9a743
Alejandro Mejia <aalejandromr@AlejandrosAir2.lan1>
2020-01-24 23:33:18 -0500

    Adds missing files

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,27 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle():
+    pass
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class Startship(FlightVehicle):
+    pass
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,11 +3,13 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def drive(self):
+        return "vroooom"
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -16,8 +18,13 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels=num_wheels)
 
-# TODO
+    def drive(self):
+        return "BRAAPP!!"
+
 
 vehicles = [
     GroundVehicle(),
@@ -29,4 +36,5 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+for i in range(len(vehicles)):
+    print(vehicles[i].drive())
